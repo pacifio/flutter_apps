59c35b96143e21d74bb84b7acbdb4f6aef2908fe
Adib Mohsin <adibmohsin.root@gmail.com>
2019-07-14 14:10:29 +0600

    added plant app scaffold

---
+++ plant_app/lib/screens/greeting_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GreetingScreen extends StatefulWidget {
+  GreetingScreen({Key key}) : super(key: key);
+
+  _GreetingScreenState createState() => _GreetingScreenState();
+}
+
+class _GreetingScreenState extends State<GreetingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
